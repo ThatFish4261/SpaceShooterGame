6ea2efb9438f7879030e5e7307ba72a43dc9eec8
ThatFish4261 <75697596+ThatFish4261@users.noreply.github.com>
2023-02-13 20:52:16 -0800

    Add project files.

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logger : MonoBehaviour
+{
+    public int health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(5.0f, 100.0f, 1.0f);
+    }
+}
